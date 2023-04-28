1bab05023f25272a500325254a79e10bbe84f271
Noa <noa.cayphas@gmail.com>
2023-04-28 16:06:41 +0200

    css mobile end

--- app.js
+++ app.js
@@ -1,44 +1,46 @@
-const html = document.createElement('html');
-html.lang = 'en';
+// const html = document.createElement('html');
+// html.lang = 'en';
 
-const head = document.createElement('head');
+// const head = document.createElement('head');
 
-const metaCharset = document.createElement('meta');
-metaCharset.setAttribute('charset', "UTF-8");
+// const metaCharset = document.createElement('meta');
+// metaCharset.setAttribute('charset', "UTF-8");
 
-const metaCompat = document.createElement('meta');
-metaCompat.setAttribute('http-equiv', "X-UA-Compatible");
-metaCompat.setAttribute('content', "IE=edge");
+// const metaCompat = document.createElement('meta');
+// metaCompat.setAttribute('http-equiv', "X-UA-Compatible");
+// metaCompat.setAttribute('content', "IE=edge");
 
-const metaViewport = document.createElement('meta');
-metaViewport.setAttribute('name', "viewport");
-metaViewport.setAttribute('content', "width=device-width, initial-scale=1.0");
+// const metaViewport = document.createElement('meta');
+// metaViewport.setAttribute('name', "viewport");
+// metaViewport.setAttribute('content', "width=device-width, initial-scale=1.0");
 
-const linkStylesheet = document.createElement('link');
-linkStylesheet.setAttribute('rel', "stylesheet");
-linkStylesheet.setAttribute('href', "assets/style.css");
+// const linkStylesheet = document.createElement('link');
+// linkStylesheet.setAttribute('rel', "stylesheet");
+// linkStylesheet.setAttribute('href', "assets/style.css");
 
-const title = document.createElement('title');
-title.textContent = "The collection";
+// const title = document.createElement('title');
+// title.textContent = "The collection";
 
-const body = document.createElement('body');
+// const body = document.createElement('body');
 
-const h1 = document.createElement('h1');
-h1.textContent = "Movie Collection";
+// const h1 = document.createElement('h1');
+// h1.textContent = "Movie Collection";
+
+
+// const script = document.createElement('script');
+// script.setAttribute('defer', "");
+// script.setAttribute('src', "app.js");
+
+// head.append(metaCharset, metaCompat, metaViewport, linkStylesheet, title);
+// body.append(h1, divCardContainer, script);
+// html.append(head, body);
+
+// document.documentElement.replaceWith(html);
+
+
 
 const divCardContainer = document.createElement('div');
-divCardContainer.setAttribute('id', "card-container");
-
-const script = document.createElement('script');
-script.setAttribute('defer', "");
-script.setAttribute('src', "app.js");
-
-head.append(metaCharset, metaCompat, metaViewport, linkStylesheet, title);
-body.append(h1, divCardContainer, script);
-html.append(head, body);
-
-document.documentElement.replaceWith(html);
-
+divCardContainer.className= "card-container";
 
 
 // array for card
@@ -48,7 +50,7 @@ const movies = [
       date: "2008",
       realisateur: "Jon Favreau",
       platforme: ["Disney+ , AppleTv"],
-      affiche : "./asset/img/iron-man.jpg",
+      affiche : "./assets/img/iron-man.jpg",
       teaser: "https://youtu.be/8ugaeA-nMTc",
       genre: ["Super-héros"],
     },
@@ -58,7 +60,7 @@ const movies = [
       date: "1956",
       realisateur: "Satyajit Ray",
       platforme: ["Prime video, Plex"],
-      affiche : "asset/img/aparajito.jpg",
+      affiche : "assets/img/aparajito.jpg",
       teaser: "https://youtu.be/mgv68E_o6VM",
       genre: ["Drama"],
     },
@@ -68,7 +70,7 @@ const movies = [
       date: "30 avril 2008",
       realisateur: "Denis Villeneuve",
       platforme: ["Canal+ , Prime video"],
-      affiche : "asset/img/BladeRunner.jpg",
+      affiche : "assets/img/BladeRunner.jpg",
       teaser: "https://youtu.be/gCcx85zbxz4",
       genre: ["Science-fiction"],
     },
@@ -78,7 +80,7 @@ const movies = [
       date: "2019",
       realisateur: "Jordan Peele",
       platforme: ["Netflix"],
-      affiche : "asset/img/us.jpg",
+      affiche : "assets/img/us.jpg",
       teaser: "https://youtu.be/hNCmb-4oXJA",
       genre: ["Horreur, Thriller"],
     },
@@ -88,7 +90,7 @@ const movies = [
       date: "2019",
       realisateur: "Bong Joon-ho",
       platforme: ["Prime video"],
-      affiche : "asset/img/parasite.jpg",
+      affiche : "assets/img/parasite.jpg",
       teaser: "https://youtu.be/5xH0HfJHsaY",
       genre: ["Thriller, Drame"],
     },
@@ -98,7 +100,7 @@ const movies = [
       date: "1997",
       realisateur: "Curtis Hanson",
       platforme: ["Disney+"],
-      affiche : "asset/img/la.confidential.jpg",
+      affiche : "assets/img/la.confidential.jpg",
       teaser: "https://youtu.be/6sOXrY5yV4g",
       genre: ["Policier"],
     },
@@ -108,7 +110,7 @@ const movies = [
       date: "2015",
       realisateur: "Danny Boyle",
       platforme: ["AppleTv"],
-      affiche : "asset/img/steve.jobs.jpg",
+      affiche : "assets/img/steve.jobs.jpg",
       teaser: "https://youtu.be/aEr6K1bwIVs",
       genre: ["Biopic, Drame"],
     },
@@ -118,7 +120,7 @@ const movies = [
       date: "1999",
       realisateur: "David Fincher",
       platforme: ["Disney+ , Prime video"],
-      affiche : "asset/img/fightclub.jpeg",
+      affiche : "assets/img/fightclub.jpeg",
       teaser: "https://youtu.be/SUXWAEX2jlg",
       genre: ["Comédie, Drame, Mystère & Thriller "],
     },
@@ -126,9 +128,9 @@ const movies = [
     {
       titre: "Le prénom",
       date: "2012",
-      realisateur: "Alexandre de La Patellière & Matthieu Delaporte",
+      realisateur: "Matthieu Delaporte",
       platforme: ["Disney+ , Canal+"],
-      affiche : "asset/img/le.prenom.jpg",
+      affiche : "assets/img/le.prenom.jpg",
       teaser: "https://youtu.be/FX2ukwKgWlo",
       genre: ["Comédie"],
     },
@@ -138,20 +140,24 @@ const movies = [
       date: "1988",
       realisateur: "Katsuhiro Ôtomo",
       platforme: ["Netflix , ADN, Prime video"],
-      affiche : "asset/img/Akira.jpg",
+      affiche : "assets/img/Akira.jpg",
       teaser: "https://youtu.be/ooKBenGK3R4",
       genre: ["Animation , Action, Drame"],
     },
 ];
 
+document.body.appendChild(divCardContainer)
+
 for (let elem of movies){
-  let x = document.createElement("div");
-  x.className = "divCards";
-  divCardContainer.appendChild(x);
+  let divCards = document.createElement("div");
+  divCards.className = "divCards";
+  console.log(divCards)
+  divCardContainer.appendChild(divCards);
+  divCardContainer.innerHTMl = divCards
 
   let divPictures = document.createElement("div");
   divPictures.className = "divPictures";
-  x.appendChild(divPictures);
+  divCards.appendChild(divPictures);
   let pictures = document.createElement("img");
   pictures.src = elem.affiche;
   pictures.setAttribute("alt", "affiche");
@@ -159,18 +165,18 @@ for (let elem of movies){
 
   let title = document.createElement("h2");
   title.textContent = elem["titre"];
-  x.appendChild(title);
+  divCards.appendChild(title);
 
   let releaseYear = document.createElement("div");
   releaseYear.className = "releaseYear";
-  x.appendChild(releaseYear);
+  divCards.appendChild(releaseYear);
   let year = document.createElement("p");
   year.textContent = elem["date"];
   year.className = "year";
 
   let divReal = document.createElement("div");
   divReal.className = "divReal";
-  x.appendChild(divReal);
+  divCards.appendChild(divReal);
   let realisation = document.createElement("p");
   realisation.textContent = elem["realisateur"];
   realisation.className = "realisation";
@@ -179,23 +185,25 @@ for (let elem of movies){
   let whereToWatch = document.createElement("p");
   whereToWatch.textContent = elem ["platforme"];
   whereToWatch.className = "platforme";
-  x.appendChild(whereToWatch);
+  divCards.appendChild(whereToWatch);
 
   let divIcon = document.createElement("div");
   divIcon.className = "divIcon";
-  x.appendChild(divIcon);
+  divCards.appendChild(divIcon);
   let trailer = document.createElement("a");
   trailer.setAttribute("target", "_blank");
   trailer.href = elem["teaser"];
   divIcon.appendChild(trailer);
-  let icon = document.createElement("img")
-  icon.src = "./asset";
+  let icon = document.createElement("img");
+  icon.src = "./assets/img/play.png";
   trailer.appendChild(icon);
 
   let gender = document.createElement("p");
   gender.textContent = elem["genre"];
   gender.className = "gender";
-  x.appendChild(gender)
+  divCards.appendChild(gender);
 };
+// console.log(movies);
+// console.log(divCards)
 
 
